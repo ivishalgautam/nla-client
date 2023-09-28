1cd54eaaa67407bbef054c00365543ecc66e91a4
Vishal Gautam <76483761+ivishalgautam@users.noreply.github.com>
2023-09-28 17:39:40 +0530

    fixed time change

--- app/lib/cookies.js
+++ app/lib/cookies.js
@@ -13,9 +13,11 @@ export function setCookie(name, value, daysToExpire) {
 // }
 
 export function getCookie(name) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(";").shift();
+  if (typeof document !== "undefined") {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(";").shift();
+  }
 }
 
 export function checkCookie(name) {
