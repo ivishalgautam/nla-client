c0386c1b03ea8afdf94145b424154a6bff65b1a3
vishal <ivishalgautamm@gmail.com>
2023-10-04 18:58:27 +0530

    added pagination

--- app/lib/requestMethods.js
+++ app/lib/requestMethods.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // const BASE_URL = "http://localhost:4000/api";
-const BASE_URL = "https://nla-server-production.up.railway.app/api";
+// const BASE_URL = "https://nla-server-production.up.railway.app/api";
+const BASE_URL = "http://193.203.163.95:4000/api";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
