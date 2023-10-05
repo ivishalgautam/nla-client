a52942054650e1a90df2e8cfed3514d6d32fd7a0
vishal <ivishalgautamm@gmail.com>
2023-10-05 16:28:13 +0530

    added pagination

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: "nlaacademy.in",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
