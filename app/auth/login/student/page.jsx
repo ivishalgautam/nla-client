3176a3b1071e76ad871dca9807a27b87c58ba958
vishal <ivishalgautamm@gmail.com>
2023-10-11 12:52:55 +0530

    added tottal income this month also expense for admin dashboard

--- app/auth/login/student/page.jsx
+++ app/auth/login/student/page.jsx
@@ -9,46 +9,12 @@ import toast from "react-hot-toast";
 export default function LoginPage() {
   const query = useSearchParams();
   const router = useRouter();
-  const [credentials, setCredentials] = useState({
-    username: "",
-    password: "",
-  });
-
-  function handleChange(e) {
-    const { name, value } = e.target;
-    setCredentials((prev) => ({ ...prev, [name]: value }));
-  }
-
-  async function handleFormSubmit(e) {
-    e.preventDefault();
-    try {
-      const { data, status } = await authRequest.post("/login", {
-        ...credentials,
-      });
-
-      if (status === 200) {
-        toast.success("Logged in successfully");
-
-        setCookie("email", data.student.email, 2);
-        setCookie("fullname", data.student.fullname, 2);
-        setCookie("student_id", data.student.id, 2);
-        setCookie("package", data.student.package, 2);
-        setCookie("grade", data.student.grade, 2);
-        setCookie("student_token", data.access_token, 2);
-
-        router.push("/student/profile");
-      }
-
-      // console.log(data);
-    } catch (error) {
-      toast.error(error.response.data.message);
-      console.log(error);
-    }
-  }
 
   useEffect(() => {
     if (getCookie("student_id")) {
       return router.push("/student/profile");
+    } else {
+      router.replace("https://nlaacademy.in/login.php");
     }
   }, []);
 
