22cc5fe16a4b08026447817751958d4036d1d618
vishal <ivishalgautamm@gmail.com>
2023-10-27 13:05:38 +0530

    fixed some bugs

--- app/student/my-tests/[testId]/page.jsx
+++ app/student/my-tests/[testId]/page.jsx
@@ -148,7 +148,7 @@ const Page = ({ params: { testId } }) => {
 
   return (
     <section>
-      <p className="text-xl font-bold mb-8 text-end fixed top-2 right-2 bg-primary text-white p-3 rounded-md">{`Time left: ${formatTime(
+      <p className="text-xl font-bold mb-8 text-end fixed bottom-2 left-2 bg-primary text-white p-3 rounded-md">{`Time left: ${formatTime(
         duration
       )}`}</p>
       <div className="grid grid-cols-6 gap-4">
