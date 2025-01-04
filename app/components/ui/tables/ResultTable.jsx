2aceecfa644ee48d4941cbaa8756c8f49ee902da
Vishal Gautam <ivishalgautamm@gmail.com>
2025-01-04 12:00:57 +0530

    added test type filteration in results

--- app/components/ui/tables/ResultTable.jsx
+++ app/components/ui/tables/ResultTable.jsx
@@ -3,8 +3,7 @@ import { getCookie } from "@/app/lib/cookies";
 import { adminRequest } from "@/app/lib/requestMethods";
 import { formatDateToIST } from "@/app/lib/time";
 import Link from "next/link";
-import { useSearchParams } from "next/navigation";
-import { parseAsInteger, useQueryState } from "nuqs";
+import { parseAsInteger, parseAsString, useQueryState } from "nuqs";
 import React, { useEffect, useState } from "react";
 import DataTable from "react-data-table-component";
 import { FiExternalLink } from "react-icons/fi";
@@ -12,12 +11,20 @@ import { FiExternalLink } from "react-icons/fi";
 export default function ResultTable() {
   const [results, setResults] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-
-  async function getResults({ page, limit }) {
+  const [page, setPage] = useQueryState("page", parseAsInteger.withDefault(1));
+  const [limit, setLimit] = useQueryState(
+    "limit",
+    parseAsInteger.withDefault(10)
+  );
+  const [type, setType] = useQueryState(
+    "type",
+    parseAsString.withDefault("olympiad")
+  );
+  async function getResults({ page, limit, type }) {
     setIsLoading(true);
     try {
       const resp = await adminRequest.get(
-        `/results?page=${page}&limit=${limit}`,
+        `/results?page=${page}&limit=${limit}&type=${type}`,
         {
           headers: { Authorization: `Bearer ${getCookie("admin_token")}` },
         }
@@ -35,7 +42,7 @@ export default function ResultTable() {
     const inputValue = e.target.value.toLowerCase();
 
     if (inputValue === "") {
-      getResults();
+      getResults({ page, limit, type });
     } else {
       const data = results?.filter((item) =>
         item.fullname.toLowerCase().includes(inputValue)
@@ -87,6 +94,7 @@ export default function ResultTable() {
     {
       name: "Created On",
       selector: (row) => formatDateToIST(row.created_at),
+      sortable: true,
     },
     {
       name: "All Results",
@@ -99,11 +107,7 @@ export default function ResultTable() {
     },
   ];
   const [totalRows, setTotalRows] = useState(0);
-  const [page, setPage] = useQueryState("page", parseAsInteger.withDefault(1));
-  const [limit, setLimit] = useQueryState(
-    "limit",
-    parseAsInteger.withDefault(10)
-  );
+
   const currentPage = parseInt(page) || 1;
   const pageSize = parseInt(limit) || 10;
 
@@ -116,12 +120,12 @@ export default function ResultTable() {
   };
 
   useEffect(() => {
-    getResults({ page, limit });
-  }, [page, limit]);
+    getResults({ page, limit, type });
+  }, [page, limit, type]);
 
   return (
     <div className="rounded">
-      <div className="mb-4 flex justify-start">
+      <div className="mb-4 flex justify-start gap-4">
         <div className="relative">
           <input
             type="text"
@@ -133,6 +137,29 @@ export default function ResultTable() {
           />
           <label htmlFor="search" className="my-label">
             Search
+          </label>
+        </div>
+        <div className="relative flex flex-col justify-end">
+          <select
+            name="test_type"
+            id="test_type"
+            onChange={(e) => {
+              setPage(1);
+              setLimit(10);
+              setType(e.target.value);
+            }}
+            className="my-input"
+            required
+            defaultValue={type}
+          >
+            <option hidden disabled value="" selected>
+              Select test type
+            </option>
+            <option value="olympiad">Olympiad</option>
+            <option value="practice">Practice</option>
+          </select>
+          <label htmlFor="test_name" className="my-label">
+            Test type
           </label>
         </div>
       </div>
@@ -148,6 +175,7 @@ export default function ResultTable() {
           paginationPerPage={pageSize}
           onChangePage={handlePageChange}
           onChangeRowsPerPage={handlePageSizeChange}
+          paginationRowsPerPageOptions={[10, 20, 30, 40, 50, 100, 200, 500]}
         />
       </div>
     </div>
